e4db08328cf9c2a546d353873bcaaa496da71a3c
Sagar Roy <sagar@freshworks.io>
2022-05-27 15:17:52 -0400

    demo application with local development setup

--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ export GIT_LOCAL_BRANCH?=$(shell git rev-parse --abbrev-ref HEAD)
 
 export GIT_LOCAL_BRANCH := $(or $(GIT_LOCAL_BRANCH),dev)
 
+# Keycloak
+export KEYCLOAK_AUTHORITY := $(or $(KEYCLOAK_AUTHORITY),https://keycloak.freshworks.club/auth/realms/social-sector-portal-test)
+export KEYCLOAK_CLIENT_ID := $(or $(KEYCLOAK_CLIENT_ID),social-sector-portal)
 
 ####################################################################
 ## Status Output
---
+++ demo-app/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dot_net.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+
+namespace dot_net.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var user = HttpContext.User;
+        if (user?.Identity.IsAuthenticated == true)
+        {
+            ViewBag.Name = user.Identity.Name;
+            return View("Private");
+        }
+        return View();
+    }
+
+    [Authorize]
+    public IActionResult Login()
+    {
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Logout() {
+        var user = HttpContext.User;
+
+        if (user?.Identity.IsAuthenticated == true)
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        return View("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
