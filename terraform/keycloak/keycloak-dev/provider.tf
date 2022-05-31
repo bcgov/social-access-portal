481df934b196845fc512fea7d82ca15a35bebec4
Simen Fivelstad Smaaberg <simen@freshworks.io>
2022-05-31 11:03:59 -0700

    BCSCP-4 Updated variable names

--- terraform/keycloak/keycloak-dev/provider.tf
+++ terraform/keycloak/keycloak-dev/provider.tf
@@ -1,8 +1,8 @@
 provider "keycloak" {
   realm         = "master"
-  client_id     = "admin-cli"
-  # client_secret = var.client_secret
-  username      = "admin"
+  client_id     = var.client_id
+  client_secret = var.client_secret
+  username      = var.username
   password      = var.password
   url           = var.keycloak_url
   base_path     = ""
