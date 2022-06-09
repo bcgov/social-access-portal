65f6c0cfbf3ed9942b34ff238e796d7bbb58ce4d
Sagar Roy <sagar@freshworks.io>
2022-06-09 14:52:04 -0400

    updated refs

--- terraform/keycloak/keycloak-dev/standard-clients/demo-app-1-5-31-2477.tf
+++ terraform/keycloak/keycloak-dev/standard-clients/demo-app-1-5-31-2477.tf
@@ -5,7 +5,7 @@ data "keycloak_authentication_flow" "demo_app_1_5_31_2477_browserflow" {
 
 
 module "demo-app-1-5-31-2477" {
-  source      = "github.com/bcgov/social-access-portal-terraform-modules?ref=b903321/modules/standard-client"
+  source      = "github.com/bcgov/social-access-portal-terraform-modules?ref=4148b67/modules/standard-client"
   realm_id    = var.standard_realm_id
   bcsc_realm_name  = var.bcsc_realm_name
   bcsc_realm_id  = var.bcsc_realm_id
